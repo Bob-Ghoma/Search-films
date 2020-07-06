ec8f59a5a0bb4d29e7a018a1d3b52314df3a89cd
Hoc <b.ghoma@gmail.com>
2020-07-06 16:28:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import movies from './movies.json';
 
 function App() {
+  const [currentMovies, setCurrentMovies] = useState("")
+
+  function searchMovies(event) {
+    setCurrentMovies(event.target.value)
+  }
+
+  function displayMovies(event) {
+    return filterMovies().map((movie, id) => <li key={id} className={movie}>{movie.name} / {movie.year} </li> )
+  }
+
+  function filterMovies() {
+    return movies.filter(movie => movie.name.toLowerCase().indexOf(currentMovies.toLowerCase()) !== -1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Afficher le nom du film</div>
+      <input id="recherche" type="texte" placeholder="Recherche" value={currentMovies} onChange={searchMovies}/>
+      <div>{displayMovies()}</div>
     </div>
   );
 }
